5c5a1bfdc2baa1df52bf027ce26a45dd76f72185
Lawal Tunde <hlawal@mycambrian.ca>
2024-07-31 11:42:57 -0400

    Class department added.

---
+++ HR.Connect.App/MVVM/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Connect.App.MVVM.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string IT { get; set; }
+        public string HumanResource { get; set; }
+        public string Marketing { get; set; }
+        public string Finance { get; set; }
+        public string Payroll {  get; set; }
+
+        public Department() { }
+    }
+}
